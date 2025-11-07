6b7e25208e0a356efb1118f3b271ea0a42df90f2
Bahadir SENYAYLA <bsenyayla@yandex.com>
2025-11-07 15:35:24 -0600

    some works

---
+++ Arge2025/AppLearn/AppCoreApi_00/Actions/CustomerActionMethods.cs
@@ -0,0 +1,51 @@
+﻿using AppCoreApi_00.DataModels;
+
+namespace AppCoreApi_00.Actions
+{
+    public class CustomerActionMethods
+    {
+        public CustomerActionMethods()
+        {
+        }
+
+        public ReservationTypeMain GetCustomerDetails()
+        {
+            List<DataModels.CustomerType> returnList = new List<DataModels.CustomerType>();
+
+            returnList.Add(new DataModels.CustomerType(){
+                Age = 4,
+                CustomerFirstName = "First Name",
+                CustomerLastName = "Last Name"
+            });
+
+            returnList.Add(new DataModels.CustomerType()
+            {
+                Age = 4,
+                CustomerFirstName = "First Name",
+                CustomerLastName = "Last Name"
+            });
+            returnList.Add(new DataModels.CustomerType()
+            {
+                Age = 4,
+                CustomerFirstName = "First Name",
+                CustomerLastName = "Last Name"
+            });
+
+
+            ReservationTypeMain reservationData = new ReservationTypeMain();
+            reservationData.Customers = returnList;
+            reservationData.PNR = "ER45CD";
+            reservationData.Date = new DateTime().Date;
+            reservationData.Ticket = new Tickets()
+            {
+                TicketNumber="14332321"
+            };
+
+
+            return reservationData;
+        }
+
+
+
+    }
+}
